eb1845524d63d6df6ea6172ee669f2d025718d9d
fpcd <fpcd28@gmail.com>
2022-03-11 18:08:24 -0500

    Finish Date class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptsPOO;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+
+//Date date1 = new Date(2022,2,11);
+//Console.WriteLine(date1);
+
+try
+{
+    Console.WriteLine(new Date(2022, 2, 28));
+    Console.WriteLine(new Date(1975, 4, 11));
+    Console.WriteLine(new Date(1985, 11, 31));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message );
+}
+
+
