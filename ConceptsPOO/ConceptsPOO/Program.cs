8b1d1f7610ec9e8819e23b8ce89558cf2ddbef53
fpcd <fpcd28@gmail.com>
2022-03-11 20:12:38 -0500

    SalaryEmployees y SalaryCommission

--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -7,15 +7,40 @@ Console.WriteLine("============");
 //Date date1 = new Date(2022,2,11);
 //Console.WriteLine(date1);
 
-try
+//try
+//{
+//    Console.WriteLine(new Date(2022, 2, 28));
+//    Console.WriteLine(new Date(1975, 4, 11));
+//    Console.WriteLine(new Date(1985, 11, 31));
+//}
+//catch (Exception error)
+//{
+//    Console.WriteLine(error.Message );
+//}
+
+Employee employee1 = new SalaryEmployee()
 {
-    Console.WriteLine(new Date(2022, 2, 28));
-    Console.WriteLine(new Date(1975, 4, 11));
-    Console.WriteLine(new Date(1985, 11, 31));
-}
-catch (Exception error)
+    Id = 1010,
+    FirstName = "Sandra",
+    LastName = "Morales",
+    BirthDate = new Date(1990, 5, 23),
+    HiringDate = new Date(2022, 1, 15),
+    IsActive = true,
+    Salary = 1815.45M
+};
+Console.WriteLine(employee1);
+
+Employee employee2 = new commissionEmployee()
 {
-    Console.WriteLine(error.Message );
-}
+    Id = 2020,
+    FirstName = "Patricia",
+    LastName = "Gutierrez",
+    BirthDate = new Date(1991, 7,23),
+    HiringDate = new Date(2022, 11, 15),
+    IsActive = true,
+    Sales = 123000M,
+    CommissionPercentaje = 0.03F
+};
+Console.WriteLine(employee2);
 
 
---
+++ ConceptsPOO/ConceptsPOO/commissionEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsPOO
+{
+    public class commissionEmployee : Employee
+    {
+        public float CommissionPercentaje { get; set; }
+
+        public decimal Sales { get; set; }
+
+        public override decimal GetValueToPay()
+        {
+            return Sales *(decimal)CommissionPercentaje;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\tCommission Percentaje :{$"{CommissionPercentaje:P2}",18}" +
+                $"\n\tSales                 :{$"{Sales:C2}",18}" +
+                $"\n\tValue to play         :{$" {GetValueToPay():C2}",18}";
+        }
+    }
+}
